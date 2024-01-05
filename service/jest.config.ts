f666814e199874c347f70fce9b7e6aa38ed46352
Filipe Melo <filipe.silva@encora.com>
2024-01-05 12:55:10 -0300

    chore: configuration & package installation

--- service/jest.config.ts
+++ service/jest.config.ts
@@ -1,15 +1,22 @@
+
+import { pathsToModuleNameMapper } from 'ts-jest';
 /*
  * For a detailed explanation regarding each configuration property and type check, visit:
  * https://jestjs.io/docs/configuration
  */
 
+import { compilerOptions } from './tsconfig.json';
+
 export default {
-    transform: {
-        '^.+\\.ts?$': 'ts-jest',
-    },
-    clearMocks: true,
-    collectCoverage: true,
-    coverageDirectory: 'coverage',
-    coverageProvider: 'v8',
-    testMatch: ['**/tests/unit/*.test.ts'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testMatch: ['**/*.spec.ts'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  })
 };
