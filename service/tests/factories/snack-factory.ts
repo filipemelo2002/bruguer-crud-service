681a49786b91d40ec293991983a574b0748282fb
Filipe Melo <filipe.silva@encora.com>
2024-01-19 10:38:05 -0300

    chore: refactor test factories & run migrations

--- service/tests/factories/snack-factory.ts
+++ service/tests/factories/snack-factory.ts
@@ -1,12 +1,12 @@
 import { Snack } from "@entities/snack";
-import { makeIngredient } from "./ingredient-factory";
+import { makeSnackIngredient } from "./snack-ingredient-factory";
 
 export function makeSnack({
   name = "Cheese Burguer",
   ingredients = [
-    makeIngredient({ name: "cheese", quantity: 2 }),
-    makeIngredient({ name: "hamburguer", quantity: 2 }),
-    makeIngredient({ name: "bread", quantity: 2 }),
+    makeSnackIngredient({ quantity: 2 }),
+    makeSnackIngredient({ quantity: 2 }),
+    makeSnackIngredient({ quantity: 2 }),
   ],
   id = "1",
 }) {
---
+++ service/tests/factories/snack-ingredient-factory.ts
@@ -0,0 +1,13 @@
+import { Ingredient } from "@entities/ingredient"
+import { SnackIngredient } from "@entities/snack-ingredient"
+
+export function makeSnackIngredient({
+  ingredient = new Ingredient({quantity: 50, name: 'lettuce'}),
+  quantity = 30,
+  id='1'
+}) {
+  return new SnackIngredient({
+    ingredient,
+    quantity: 2
+  }, id)
+}
